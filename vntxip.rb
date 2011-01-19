e9ab24796ea26ad1219f9bbd6f12da399bf8bdb5
Graham McIntire <gmcintire@gmail.com>
2011-01-19 08:18:38 -0600

    changed ip detection

--- vntxip.rb
+++ vntxip.rb
@@ -2,9 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  raw = @request.env["REMOTE_ADDR"]
-  raw.match(/^(\d+\.\d+\.\d+\.\d+),?/)
-  @ip = $1
-  haml '=@ip'
+  request.env['REMOTE_ADDR'].split(",").first
 end
 
